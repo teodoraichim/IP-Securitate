6bc4cba81073261b3c3cab0ed44cb610b0ab4629
Mariuta Silviu-Andrei <silviu.andrei53m@gmail.com>
2019-05-14 00:20:03 +0300

    Update Controller.java

--- IP-Security/Final/src/main/java/com/security/Controller.java
+++ IP-Security/Final/src/main/java/com/security/Controller.java
@@ -3,6 +3,8 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -10,19 +12,30 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class Controller {
 
     private static final String template = "Hello, %s!";
-//    private final AtomicLong counter = new AtomicLong();
+    //    private final AtomicLong counter = new AtomicLong();
 //
     @RequestMapping(value="/login",method=POST)
-    public boolean login(@RequestParam(value="username") String name,@RequestParam(value="password") String pass) {
-        User user=new User(String.format(template,name),String.format(template,pass),"");
-        return user.login();
+    public String login(@RequestParam(value="username") String name,@RequestParam(value="password") String pass,@RequestHeader("Authorization") String auth) {
+        User user=new User(name,pass,"");
+        System.out.println("-"+name+"-"+pass+"-");
+        if(user.login()) return user.createSession();
+        return "false";
     }
-//    @RequestMapping(value="/register",method=POST)
-//    {
-//    }
-//
-//    @RequestMapping(value="/activate",method=POST)
-//
+    @RequestMapping(value="/register",method=POST)
+    public boolean register(@RequestParam(value="username") String name,@RequestParam(value="password") String pass,@RequestParam(value="mail")String mail) {
+        User user=new User(String.format(template,name),String.format(template,pass),"");
+        try {
+            return user.register();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    @RequestMapping(value="/activate",method=POST)
+    public boolean activate(@RequestParam(value="username") String name,@RequestParam(value="password") String pass,@RequestParam(value="code") String code) {
+        User user=new User(String.format(template,name),String.format(template,pass),"");
+        return user.activate(code);
+    }
 
 
 }
